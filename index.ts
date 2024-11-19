35aba84d6550a941200f4a6cd622edd560b602fa
parthsingh <parthsingh1910>
2024-11-19 12:46:03 +0530

    Sending simple message

--- index.ts
+++ index.ts
@@ -1,2 +1,27 @@
-const greeting: string = "Hello, TypeScript in Node.js!";
-console.log(greeting);
+import Express, { NextFunction, Request, Response } from 'express';
+import { Server } from "socket.io"
+const app = Express()
+
+
+const PORT = 8000
+
+const server = app.listen(PORT, () => {
+  console.log("TESTING")
+  console.log(`I am running in ${PORT}`)
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const io = new Server(server);
+
+io.on('connection', function (socket) {
+  socket.emit('greeting-from-server', {
+    greeting: 'Hello Client'
+  });
+  socket.on('greeting-from-client', function (message) {
+    console.log(message);
+  });
+});
+
