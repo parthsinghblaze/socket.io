9e0a70a6bcc5a1cef15aa5454694becd9c7ca678
parthsingh <parthsingh1910>
2024-11-19 14:10:04 +0530

    Disconnecting the socket

--- index.ts
+++ index.ts
@@ -26,6 +26,17 @@ io.on('connection', function (socket) {
       sockets[i].send(message);
     }
   });
+
+  socket.on('disconnect', function () {
+    for (var i = 0; i < sockets.length; i++) {
+      if (sockets[i].id === socket.id) {
+        sockets .splice(i, 1);
+      }
+    }
+    console.log('The socket disconnected. There are ' +
+      sockets.length + ' connected sockets');
+  });
+
   socket.emit('greeting-from-server', {
     greeting: 'Hello Client'
   });
