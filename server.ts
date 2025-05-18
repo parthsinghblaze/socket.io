6b657fa1aff4fc5287defc240891398c63ee4cb3
parth <parthsingh191098@gmail.com>
2025-05-18 12:24:54 +0530

    Connection of the server and a html page

---
+++ server.ts
@@ -0,0 +1,25 @@
+import Express, { NextFunction, Request, Response } from 'express';
+import { Server } from "socket.io"
+const app = Express()
+
+
+const PORT = 8000
+
+const server = app.listen(PORT, () => {
+    console.log("TESTING")
+    console.log(`I am running in ${PORT}`)
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
